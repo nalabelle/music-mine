831dc536b03466b44578ef5fc607d67894059829
nalabelle <nalabelle@gmail.com>
2015-05-02 14:42:37 -0700

    loop the reviews files

--- processing/htmlprocessing.py
+++ processing/htmlprocessing.py
@@ -2,6 +2,7 @@
 from bs4 import BeautifulSoup
 import sys
 import sentiment
+import os
 
 def getTextFromHTML(html):
   soup = BeautifulSoup(html)
@@ -15,9 +16,15 @@ def getTextFromHTML(html):
 if __name__ == "__main__":
   path = sys.argv[1]
   print "path: " + path
-  f = open(path, 'r')
-  html = f.read()
-  text = getTextFromHTML(html)
-  pos, neg = sentiment.getSentiment(text)
-  print pos, neg
-  f.close()
+  for subdir, dirs, files in os.walk(path):
+    for file in files:
+      fileName, fileExtension = os.path.splitext(file)
+      if fileExtension != ".txt":
+        continue
+      fpath = os.path.join(subdir, file)
+      f = open(fpath, 'r')
+      html = f.read()
+      text = getTextFromHTML(html)
+      pos, neg = sentiment.getSentiment(text)
+      print pos, neg
+      f.close()
